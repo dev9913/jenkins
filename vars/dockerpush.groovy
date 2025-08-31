e3d36c3ab6fc7d98faa8f1d1ea7e9e8ad052f50e
DEV JANGIR <11ganeshji11@gmail.com>
2025-08-31 17:02:54 +0530

    Update dockerpush.groovy

--- vars/dockerpush.groovy
+++ vars/dockerpush.groovy
@@ -1,10 +1,10 @@
-def call(String imgName, String tag) {
+def call(String imgName, String tag, String userName) {
     withCredentials([usernamePassword(credentialsId: 'DockerCred',
                                       usernameVariable: 'DOCKER_USER',
                                       passwordVariable: 'DOCKER_PASS')]) {
         sh """
             docker login -u "${DOCKER_USER}" -p "${DOCKER_PASS}" 
-            docker push "${DOCKER_USER}"/"${imgName}":"${tag}"
+            docker push "${userName}"/"${imgName}":"${tag}"
         """
     }
 }
