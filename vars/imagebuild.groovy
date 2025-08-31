fa9aef638f83c56aa92656ce9cd37c3262cf2c63
DEV JANGIR <11ganeshji11@gmail.com>
2025-08-31 17:16:48 +0530

    Update imagebuild.groovy

--- vars/imagebuild.groovy
+++ vars/imagebuild.groovy
@@ -1,5 +1,5 @@
-def call( String imgName , String tag) {
+def call( String imgName , String tag, String userName) {
   echo 'Building a Docker image...'
-  sh "docker build -t ${imgName}:${tag} ."
+  sh "docker build -t ${userName}/${imgName}:${tag} ."
   echo "Docker Image Build Success."
 }  
