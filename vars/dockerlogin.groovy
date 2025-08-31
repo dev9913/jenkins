1fd6c6a4070a8ff369079f4c5eed7b05eefb0ece
DEV JANGIR <11ganeshji11@gmail.com>
2025-08-31 16:29:05 +0530

    Update dockerlogin.groovy

--- vars/dockerlogin.groovy
+++ vars/dockerlogin.groovy
@@ -1,5 +1,7 @@
-def call(String mydockerhubuser , String password){
-   echo " Docker login "
-   sh " docker login -u ${mydockerhubuser} -p ${password} "
-   echo " Docker login Successfully. "
+def call(String mydockerhubuser, String password) {
+    echo "Docker login ..."
+    sh """
+        echo '${password}' | docker login -u '${mydockerhubuser}' --password-stdin
+    """
+    echo "Docker login Successfully."
 }
