10c16cc42414ff0a6795ac4c0f309488d036cacc
DEV JANGIR <11ganeshji11@gmail.com>
2025-08-31 17:21:59 +0530

    Update dockerpush.groovy

--- vars/dockerpush.groovy
+++ vars/dockerpush.groovy
@@ -1,5 +1,5 @@
 def call(String imgName, String tag, String userName) {
-    withCredentials([usernamePassword(credentialsId: 'DockerCred,
+    withCredentials([usernamePassword(credentialsId: 'DockerCred'
                                       usernameVariable: 'DOCKER_USER',
                                       passwordVariable: 'DOCKER_PASS')]) {
         sh "docker login -u ${DOCKER_USER} -p ${DOCKER_PASS}" 
