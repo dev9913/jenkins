0db4d60a4149780e656fcfc2cb60f83d590542b2
DEV JANGIR <11ganeshji11@gmail.com>
2025-08-31 15:34:10 +0530

    Update runnigImage.groovy

--- vars/runnigImage.groovy
+++ vars/runnigImage.groovy
@@ -1,4 +1,4 @@
-def call(String imgName , String tag ,String port){
+def call(String imgName , String tag ,int port){
     echo 'Running the container on browser...'
     sh "docker run -d -p ${port}:${port} ${imgName}:${tag}"
     echo "Container Running Success."
